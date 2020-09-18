dbc1872dd128a6e1359f6b2b8d3ec1468c49782c
Marisol <marisol.lince@gmail.com>
2020-09-18 09:38:10 +0000

    created checkout app and models

--- boutique_krister/settings.py
+++ boutique_krister/settings.py
@@ -48,6 +48,11 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -61,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_krister.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,8 +84,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'bag.contexts.bag_contents'
+                'django.template.context_processors.media',
+                'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
